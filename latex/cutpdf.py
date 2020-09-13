1b7416e1b82c4e1749d701ee590c9614700b7f43
yzr95924 <zryang@cse.cuhk.edu.hk>
2020-09-14 00:48:01 +0800

    update latex script

--- latex/cutpdf.py
+++ latex/cutpdf.py
@@ -1,1 +1,37 @@
-#
+#!/usr/bin/python3
+# auto pdfcrop all pdf figures under a given dir
+import argparse
+from os import listdir
+from os import path
+import os 
+
+def GetAllPDF(dir):
+    pdfList = []
+    for file in listdir(dir):
+        if (file.endswith(".pdf")):
+            pdfList.append(path.join(dir, file))
+    return pdfList
+
+def CropPDF(inputFile):
+    cmd = "pdfcrop " + inputFile
+    print(cmd)
+    os.system(cmd)
+    cropFileName = inputFile[:-4] + "-crop.pdf"
+    print(cropFileName)
+    cmd = "mv " + cropFileName + " " + inputFile
+    print(cmd)
+    os.system(cmd)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='auto pdfcrop all pdf figures under a given dir')
+    parser.add_argument('-d', metavar='Directory', nargs=1, help='the path of input directory')
+    parser.print_help()
+    args = parser.parse_args()
+    inputDir = str(args.d[0])
+
+    pdfList = GetAllPDF(inputDir)
+    print(pdfList) 
+    for pdf in pdfList:
+        CropPDF(pdf)
+    print("Done")
