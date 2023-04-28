3eab2d17a64a681df3a6b5194729cc82babf6f14
yzr95924 <yzr95924@gmail.com>
2023-04-29 03:08:40 +0800

    update vimrc

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,5 +1,12 @@
 # set up the vim config
+echo "Set up the vim colors"
 touch ~/.vimrc
 cp vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 cp molokai.vim ~/.vim/colors/
+echo "Done"
+
+echo "Set up plug.vim"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Done"
