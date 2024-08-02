df391bd2cefa8e287c0a7c067c161d9bad210336
yzr95924 <yzr95924@gmail.com>
2024-08-03 02:27:18 +0800

    UPDATE: add clangd script

--- nvim/install_astronvim.bash
+++ nvim/install_astronvim.bash
@@ -1,2 +1,6 @@
 #!/bin/bash
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+
+echo "start to install clangd"
+npm install clangd
+cp ./clangd.lua ~/.config/nvim/lua/plugins/clangd.lua
